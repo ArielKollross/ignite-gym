078e52f580f9dfe3a7de8177547ab4ffcd16a7b4
Ariel Kollross <arielkollross@gmail.com>
2025-01-15 00:02:07 -0300

    feat: Add new components to home screen

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,14 +1,18 @@
-import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { Heading, HStack, Text, VStack, Icon } from "@gluestack-ui/themed";
 import { UserPhoto } from "./UserPhoto";
+import { LogOut } from "lucide-react-native"
 
 export function HomeHeader() {
     return (
         <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap={"$4"}>
             <UserPhoto source={{ uri: 'https://github.com/arielkollross.png' }} alt="Imagem do Usuário" w="$16" h="$16" />
-            <VStack>
+
+            <VStack flex={1}>
                 <Text color="$gray100" fontSize="$sm">Home</Text>
                 <Heading color="$gray100" fontSize="$md">Ariel Kollross</Heading>
             </VStack>
+
+            <Icon as={LogOut} color="$gray200" size="xl" />
         </HStack >
     );
 }
